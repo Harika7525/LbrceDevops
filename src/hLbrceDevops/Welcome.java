e048150ea232834bb9d96a887eea15702e7770f3
AdmiN <AdmiN@ITWS000>
2024-08-21 14:56:33 +0530

    welcome

---
+++ src/hLbrceDevops/Welcome.java
@@ -0,0 +1,21 @@
+package hLbrceDevops;
+
+public class Welcome {
+
+
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+
+		System.out.println("welcome");
+
+		
+
+
+
+	}
+
+
+
+}
